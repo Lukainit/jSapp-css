8fc0e5041ffa340fd154be5f8f93a9adce64d770
hpxxph <hpxxph@MacBook-Air-Nazar.local>
2022-12-13 17:15:29 +0200

    check js plsЖ)

--- project2/js/app.js
+++ project2/js/app.js
@@ -1,28 +1,50 @@
+let howOldThisUser
+
 function searchRealAge () {
 
-    let howOldThisUser = +prompt('how old r u?', ''); 
+    howOldThisUser = prompt('how old r u?', ''); 
 
-    if (howOldThisUser >= 18) {
-        console.log('u can sigh in:)')  
+    while (howOldThisUser == '' || howOldThisUser == null || isNaN(howOldThisUser)) {
+
+        howOldThisUser = prompt('how old r u?, type pls number, if u dont type your age:) i hack u', ''); 
+
+        while (howOldThisUser == '' || howOldThisUser == null || isNaN(howOldThisUser)) {
+
+            alert('i warned you')
+
+            howOldThisUser = prompt('ENTER YOUR AGE!!!', '')
+
+            while (howOldThisUser == '' || howOldThisUser == null) {
+
+                for (let i = 0; i < 99999; i++) {
+
+                    alert('all right)');
+
+                }
+
+            }
+
+        }
+    }
+
+    if (howOldThisUser == null || howOldThisUser == undefined) {
+        console.log('this user hack this website!!!')
+    } else if (howOldThisUser > 18 ) {
+        console.log('u can sigh in:)')
     } else if (howOldThisUser < 18 ) {
         console.log('person very young')
-    } else if (howOldThisUser <= 100 ) {
+    } else if (howOldThisUser > 50 ) {
         console.log('old person. wtf?')
     } else {
-        console.log('error404')
+
     } 
 
     const userDB = {
         realAgeThisPerson: howOldThisUser
-    }; 
-
+    }
+    
     console.log(userDB); 
 
-    
-    
-}; 
+}
 
-searchRealAge();  
-
-
-
+searchRealAge() 
